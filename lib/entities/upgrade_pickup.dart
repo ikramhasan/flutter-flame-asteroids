d87a6877e667cfd55bf3a2f904eb2be1911003e4
Ikram Hasan <ikramhasan.dev@gmail.com>
2026-02-04 04:45:54 +0600

    feat: Implement a pulsing visual effect and slightly increase the size for the `UpgradePickup` entity.

--- lib/entities/upgrade_pickup.dart
+++ lib/entities/upgrade_pickup.dart
@@ -1,22 +1,39 @@
+import 'dart:math';
+
 import 'package:asteroids/config/game_assets.dart';
 import 'package:asteroids/game/asteroids_game.dart';
 import 'package:asteroids/entities/ship.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 
 /// Collectible that upgrades the ship's weapons.
 class UpgradePickup extends SpriteComponent
     with CollisionCallbacks, HasGameReference<AsteroidsGame> {
   UpgradePickup({required super.position})
       : super(
-          size: Vector2.all(28),
+          size: Vector2.all(32),
           anchor: Anchor.center,
         );
+
+  double _pulseTime = 0;
 
   @override
   Future<void> onLoad() async {
     sprite = await Sprite.load(GameAssets.upgradeIcon);
+    paint = Paint()
+      ..colorFilter =
+          const ColorFilter.mode(Colors.greenAccent, BlendMode.modulate);
     add(CircleHitbox());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    _pulseTime += dt;
+    final pulse = 1 + sin(_pulseTime * 4) * 0.06;
+    scale = Vector2.all(pulse);
   }
 
   @override
